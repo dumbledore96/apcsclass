7072cd024593e38860c6ba8a854e555f0848bc9d
YaoDumbledore <95558265+dumbledore96@users.noreply.github.com>
2022-09-27 12:00:39 +0800

    Add files via upload

---
+++ a288.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rep()
+{
+    string s;
+    while(cin>>s){
+        if(s=="IF"){
+            return rep();
+        }
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--){
+
+    }
+}
